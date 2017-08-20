4d37656b47687467aa60cea62dacf283b2b7e12f
sasayabaku <sasayabaku@yahoo.co.jp>
2017-08-20 18:22:25 +0900

    exam_rnnが完成 しかし，学習データが25step分しかないので未来予想が上手にできる可能性がかなり低い．

--- exam_cnn.py
+++ exam_cnn.py
@@ -1,6 +1,7 @@
 # encoding:utf-8
 
 # import numpy as np
+# import sys
 # import matplotlib.pyplot as plt
 from keras.models import Sequential
 from keras.layers.convolutional import Conv2D
@@ -13,7 +14,7 @@ from keras.callbacks import TensorBoard
 
 from keras.datasets import cifar10
 from keras.utils import np_utils
-
+import pydot
 
 # NUM_CLASSES = 50
 
--- exam_rnn.py
+++ exam_rnn.py
@@ -1,16 +1,17 @@
 # encoding: utf-8
 
-# 未完成
-
 from keras.models import Sequential
 from keras.layers.core import Dense, Activation
 from keras.layers.recurrent import LSTM
 from keras.optimizers import Adam
+from keras.callbacks import EarlyStopping
 
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
+import pandas as pd
+import math
+import random
 
 
 def sin(x, T=100):
@@ -35,37 +36,60 @@ def make_dataset(low_data, n_prev=100):
 
     return re_data, re_target
 
-
-
 f = toy_problem()
 
 g, h = make_dataset(f)
 
-X_train, X_test, Y_train, Y_test = train_test_split(g, h, test_size=0.1)
+future_test = g[175].T
 
-print(X_train.shape)
-print(X_test.shape)
-print(Y_train.shape)
-print(Y_test.shape)
-print(len(f))
+# 1つの学習データの時間の長さ -> 25
+time_length = future_test.shape[1]
+# 未来の予測データを保存していく変数
+future_result = np.empty((0))
 
-length_of_sequence = len(g)
+length_of_sequence = g.shape[1]
 in_out_neurons = 1
 n_hidden = 300
 
+# モデル構築
 model = Sequential()
 model.add(LSTM(n_hidden, batch_input_shape=(None, length_of_sequence, in_out_neurons), return_sequences=False))
 model.add(Dense(in_out_neurons))
 model.add(Activation("linear"))
-
 optimizer = Adam(lr=0.001)
 model.compile(loss="mean_squared_error", optimizer=optimizer)
 
-# model.fit(X_train, Y_train, batch_size=600, nb_epoch=15, validation_data=(X_test, Y_test), callbacks=["early_stopping"])
-model.fit(g, h, batch_size=300, nb_epoch=15, validation_split=0.1, callbacks=["early_stopping"])
+# 学習
+early_stopping = EarlyStopping(monitor='val_loss', mode='min', patience=20)
+model.fit(g, h,
+          batch_size=300,
+          epochs=100,
+          validation_split=0.1,
+          callbacks=[early_stopping]
+          )
 
-# ノイズ付きサイン波をプロット
-# plt.figure()
-# plt.plot(f)
-# plt.show()
+# 予測
+predicted = model.predict(g)
+
+
+
+# 未来予想
+for step2 in range(400):
+
+    test_data = np.reshape(future_test, (1, time_length, 1))
+    batch_predict = model.predict(test_data)
+
+    future_test = np.delete(future_test, 0)
+    future_test = np.append(future_test, batch_predict)
+
+    future_result = np.append(future_result, batch_predict)
+
+
+# sin波をプロット
+plt.figure()
+plt.plot(range(25,len(predicted)+25),predicted, color="r", label="predict")
+plt.plot(range(0, len(f)), f, color="b", label="row")
+plt.plot(range(0+len(f), len(future_result)+len(f)), future_result, color="g", label="future")
+plt.legend()
+plt.show()
 
